37f806dc99447fa9750dd896f40d548a6dc21d9c
Mario Araya <mario.araya@globant.com>
2018-08-14 09:28:41 -0300

    first test passing OK

--- test/test-server.js
+++ test/test-server.js
@@ -6,7 +6,14 @@ var should = chai.should();
 chai.use(chaiHttp)
 
 describe('Blobs tests', function(){
-    it('should list ALL blobs on /blobs GET');
+    it('should list ALL blobs on /blobs GET', function(done){
+        chai.request(server)
+            .get('/blobs')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
     it('should list a SINGLE blob on /blob/<id> GET');
     it('should add a SINGLE blob on /blobs POST');
     it('should update a SINGLE blob on /blob/<id> PUT');
